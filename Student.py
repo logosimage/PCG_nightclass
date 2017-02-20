5ec8b0fd36af532581031e28b481ab5e0d17b0e4
Larry <Larry>
2017-02-19 23:00:13 -0800

    PCG_class Lab

---
+++ Student.py
@@ -0,0 +1,36 @@
+'''
+Create a class called student
+Every student has the following instance variables
+name --> String --> Declared in constructor
+year(aka year in school) --> Integer --> Declared in constructor
+grade(current course grade aka 88% ect..) --> float--> Initialize to 0.0
+scores --> List ---> Initialize to empty
+Create the following functions
+get_name
+get_year
+get_scores --> sum of the list / 100 (To find the sum use a for loop)
+'''
+
+Class student:
+def__init__(self,name,year):
+
+	self.name = name
+
+	self.year = year
+
+	self.grade = 0.0
+
+	self.score = []
+
+def name(self):
+	return get_name
+def year(self):
+	return get_year
+def grade(self):
+	return get_grade
+def score(self):
+	return get_score
+
+__init__
+
+
